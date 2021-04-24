103a5b018ab331746c4f12271db0726e69a3ffb5
Türker Öntaşçı <turkerontasci@hotmail.com>
2021-04-25 01:38:01 +0300

    Category card

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,63 +1,5 @@
 import 'package:magazapp_flutter/products/product.dart';
-
-import 'reusable_card.dart';
 import 'package:flutter/material.dart';
-
-class CategoryCard extends StatelessWidget {
-  CategoryCard({
-    @required this.assetImage,
-    this.categoryTitle,
-    this.cardHeigth,
-    this.cardWidth,
-    this.fontSize,
-    this.onPress,
-  });
-
-  final AssetImage assetImage;
-  final String categoryTitle;
-  final double cardHeigth;
-  final double cardWidth;
-  final double fontSize;
-  final Function onPress;
-
-  @override
-  Widget build(BuildContext context) {
-    return ReusableCard(
-      onPress: onPress,
-      cardHeigth: cardHeigth,
-      cardWidth: cardWidth,
-      cardChild: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          image: DecorationImage(image: assetImage, fit: BoxFit.cover),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              child: Text(
-                "$categoryTitle",
-                style: TextStyle(
-                  shadows: [
-                    Shadow(
-                      blurRadius: 10.0,
-                      color: Colors.black,
-                      offset: Offset(5.0, 5.0),
-                    ),
-                  ],
-                  fontSize: fontSize,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class RoundIconButton extends StatelessWidget {
   RoundIconButton(
@@ -88,3 +30,68 @@ class RoundIconButton extends StatelessWidget {
     );
   }
 }
+
+class CategoryCard extends StatelessWidget {
+  CategoryCard(
+      {@required this.color,
+        @required this.assetImage,
+        @required this.categoryTitle,
+        @required this.fontSize,
+        this.onPress,
+        @required this.cardWidth,
+        @required this.cardHeigth});
+
+  final AssetImage assetImage;
+  final double fontSize;
+  final String categoryTitle;
+  final Color color;
+  final Function onPress;
+  final double cardWidth;
+  final double cardHeigth;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        width: cardWidth,
+        height: cardHeigth,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(image: assetImage, fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                child: Text(
+                  "$categoryTitle",
+                  style: TextStyle(
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black,
+                        offset: Offset(5.0, 5.0),
+                      ),
+                    ],
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        margin: EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -17,105 +17,125 @@ class _CategoryScreenState extends State<CategoryScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Container(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: <Widget>[
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Kadın Giyim",
-                        assetImage: AssetImage('images/elbise.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Aksesuar",
-                        assetImage: AssetImage('images/canta.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Elektronik",
-                        assetImage: AssetImage('images/wdblack.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Müzik",
-                        assetImage: AssetImage('images/guitar.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Spor",
-                        assetImage: AssetImage('images/nike.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Ev - Yaşam",
-                        assetImage: AssetImage('images/mobilya.jpeg'),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Erkek Giyim",
-                        assetImage: AssetImage('images/erkekgiyim.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Kişisel\nBakım",
-                        assetImage: AssetImage('images/kisiselbakim.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Outdoor",
-                        assetImage: AssetImage('images/outdoor2.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Kitap",
-                        assetImage: AssetImage('images/kitap.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Kırtasiye",
-                        assetImage: AssetImage('images/kalem.jpeg'),
-                      ),
-                      CategoryCard(
-                        fontSize: fontSize,
-                        cardWidth: cardWidth,
-                        cardHeigth: cardHeigth,
-                        categoryTitle: "Pet Shop",
-                        assetImage: AssetImage('images/kedigopek.jpeg'),
-                      ),
-                    ],
-                  ),
-                ],
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Kadın Giyim",
+                          assetImage: AssetImage('images/elbise.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Erkek Giyim",
+                          assetImage: AssetImage('images/erkekgiyim.jpeg'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Aksesuar",
+                          assetImage: AssetImage('images/canta.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Elektronik",
+                          assetImage: AssetImage('images/wdblack.jpeg'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Ev - Yaşam",
+                          assetImage: AssetImage('images/mobilya.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Kişisel\nBakım",
+                          assetImage: AssetImage('images/kisiselbakim.jpeg'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Müzik",
+                          assetImage: AssetImage('images/guitar.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Spor",
+                          assetImage: AssetImage('images/nike.jpeg'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Outdoor",
+                          assetImage: AssetImage('images/outdoor2.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Kitap",
+                          assetImage: AssetImage('images/kitap.jpeg'),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Kırtasiye",
+                          assetImage: AssetImage('images/kalem.jpeg'),
+                        ),
+                        CategoryCard(
+                          fontSize: fontSize,
+                          cardWidth: cardWidth,
+                          cardHeigth: cardHeigth,
+                          categoryTitle: "Pet Shop",
+                          assetImage: AssetImage('images/kedigopek.jpeg'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
